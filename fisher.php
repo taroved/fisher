bf147022461e24443373fd3e9aa4543f2b6d3098
taroved <anymetto@gmail.com>
2014-12-06 19:20:51 +0300

    fixed strategy

--- fisher.php
+++ fisher.php
@@ -1,12 +1,16 @@
 <?php
 $play = null; //we can use singleton but we don't do it now :)
 
+$log_file = 'fisher.log';
 function trace($msg)
 {
-    global $play;
+    global $play, $log_file;
+    $str = "";
     if ($play)
-        echo '['.$play->state_string().'] ';
-    echo $msg."\n";
+        $str .= '['.$play->state_string().'] ';
+    $str .= $msg."\n";
+    echo $str;
+    file_put_contents($log_file, $str, FILE_APPEND);
     flush();
 }
 
@@ -85,16 +89,17 @@ class Play
   function state_string()
   {
       $sides = array(
-          $this->current_side() => array('Fisher' => 0, 'Ancestor' => 0, 'Child' => 0),
-          $this->front_side() => array('Fisher' => 0, 'Ancestor' => 0, 'Child' => 0)
+          self::RIVERSIDE_LEFT => array('Fisher' => 0, 'Ancestor' => 0, 'Child' => 0),
+          self::RIVERSIDE_RIGHT => array('Fisher' => 0, 'Ancestor' => 0, 'Child' => 0)
       );
       foreach ($this->riversides as $side_id => $riverside)
           foreach ($riverside as $passenger)
               foreach ($sides[$side_id] as $class => &$count)
                   if (is_a($passenger, $class))
                       $count++;
-      return sprintf('%df%dp%dc  %df%dp%dc', $sides[$this->current_side()]['Fisher'], $sides[$this->current_side()]['Ancestor'], $sides[$this->current_side()]['Child'],
-          $sides[$this->front_side()]['Fisher'], $sides[$this->front_side()]['Ancestor'], $sides[$this->front_side()]['Child']);
+      return sprintf('%df%dp%dc%s%df%dp%dc', $sides[self::RIVERSIDE_LEFT]['Fisher'], $sides[self::RIVERSIDE_LEFT]['Ancestor'], $sides[self::RIVERSIDE_LEFT]['Child'],
+          $this->boat->side == self::RIVERSIDE_LEFT ? '< ' : ' >',
+          $sides[self::RIVERSIDE_RIGHT]['Fisher'], $sides[self::RIVERSIDE_RIGHT]['Ancestor'], $sides[self::RIVERSIDE_RIGHT]['Child']);
   }
 }
 
@@ -189,10 +194,6 @@ class Boat
 
 
 
-trace('//////////////////////////////////');
-trace('/* solving with canonical rules */');
-trace('//////////////////////////////////');
-
 $play = new Play(
     array(
         new Fisher(),
@@ -205,6 +206,10 @@ $play = new Play(
     ),
     array()
 );
+trace('//////////////////////////////////');
+trace('/* solving with canonical rules */');
+trace('//////////////////////////////////');
+
 //start: 1f 2p 2c (1 fisher, 2 parents, 2 children) on left side <--> 0 passengers on right side
 $play->turn(0, 0, 2);//f2p   2c-->  2c    \
 $play->turn(0, 0, 1);//f2p1c 1c<--  1c    | 1. In this step we moved first parent to another side.
@@ -238,59 +243,55 @@ class PowerPlay extends Play
     function run_stratagy()
     {
         try {
-            $children_count = 0;
+            $parents_count = count(array_filter($this->riversides[self::RIVERSIDE_LEFT], function ($passenger) {
+                return is_a($passenger, 'Ancestor');
+            }));
+            $children_count = count(array_filter($this->riversides[self::RIVERSIDE_LEFT], function ($passenger) {
+                return is_a($passenger, 'Child');
+            }));
+
             //move parents
-            foreach ($this->riversides[self::RIVERSIDE_LEFT] as $passenger)
-                if (is_a($passenger, 'Ancestor'))
-                    $this->move_parent();
-                elseif (is_a($passenger, 'Child'))
-                    $children_count ++;
+            for ($i=0; $i<$parents_count; $i++)
+                $this->move_parent();
+
+            print_r($children_count);
             //move all childs except last 2
-            foreach ($this->riversides[self::RIVERSIDE_LEFT] as $passenger)
-                if (is_a($passenger, 'Ancestor') && $children_count >= 2) {
-                    $this->move_parent();
-                    $children_count --;
-                }
+            for ($i=2; $i<$children_count; $i++)
+                $this->move_child();
+
             //final accord
+            $this->move_fisher();
             $this->move_2child_and_fisher_back();
         }
         catch (Exception $e) {
-            trace('Strategy:' . $e->getMessage());
+            trace($e->getMessage());
             exit;
         }
     }
 
     function enough($curside_fishers, $curside_parents, $curside_childs, $frontside_fishers, $frontside_parents, $frontside_childs)
     {
-        $sides = array(
-            $this->current_side() => array('Fisher' => 0, 'Ancestor' => 0, 'Child' => 0),
-            $this->front_side() => array('Fisher' => 0, 'Ancestor' => 0, 'Child' => 0)
-        );
-        foreach ($this->riversides as $side_id => $riverside)
-            foreach ($riverside as $passenger)
-                foreach ($sides[$side_id] as $class => &$count)
-                    if (is_a($passenger, $class))
-                        $count++;
-        $required_sides = array(
-            $this->current_side() => array('Fisher' => $curside_fishers, 'Ancestor' => $curside_parents, 'Child' => $curside_childs),
-            $this->front_side() => array('Fisher' => $frontside_fishers, 'Ancestor' => $frontside_parents, 'Child' => $frontside_childs)
-        );
-        foreach ($required_sides as $side_id => $counts)
-            foreach ($counts as $class => $count)
-                if ($sides[$side_id][$class] < $required_sides[$side_id][$class])
-                    return false;
+        $map = array(array('Fisher', $this->current_side()), array('Ancestor', $this->current_side()), array('Child', $this->current_side()),
+            array('Fisher', $this->front_side()), array('Ancestor', $this->front_side()), array('Child', $this->front_side()));
+
+        $input_vector = array($curside_fishers, $curside_parents, $curside_childs, $frontside_fishers, $frontside_parents, $frontside_childs);
+        $counts_vector = array(0, 0, 0, 0, 0, 0);
+
+        foreach ($this->riversides as $side_id => $passengers)
+            foreach ($passengers as $passenger)
+                foreach ($map as $idx => $m)
+                    if (is_a($passenger, $m[0]) && $side_id==$m[1])
+                        $counts_vector[$idx]++;
+
+        foreach ($input_vector as $key => $count)
+            if ($count > $counts_vector[$key])
+                return false;
         return true;
     }
 
     function move_parent()
     {
-        if ($this->enough(0,1,1,0,0,1))
-        {
-            $this->turn(0, 1, 0);//f1p1c 1p-->  1p1c  | The Boat is in the same state.
-            $this->turn(0, 0, 1);//f1p2c 1c<--  1p    /
-            trace('Strategy: 2 turns step: parent moved');
-        }
-        elseif ($this->enough(0,1,2,0,0,0))
+        if ($this->enough(0,1,2,0,0,0))
         {
             $this->turn(0, 0, 2);//f2p   2c-->  2c    \
             $this->turn(0, 0, 1);//f2p1c 1c<--  1c    | 1. In this step we moved first parent to another side.
@@ -300,18 +301,12 @@ class PowerPlay extends Play
         }
         else
         {
-            throw new Exception('Not enough passengers on riversides');
+            throw new Exception('Step failed: Not enough passengers on riversides');
         }
     }
     function move_fisher()
     {
-        if ($this->enough(1,0,1,0,0,1))
-        {
-            $this->turn(1, 0, 0);//1c    f -->  f2p1c | The Boat is in the same state.
-            $this->turn(0, 0, 1);//2c    1c<--  f2p   /
-            trace('Strategy: 2 turns step: fisher moved');
-        }
-        elseif ($this->enough(1,0,2,0,0,0))
+        if ($this->enough(1,0,2,0,0,0))
         {
             $this->turn(0, 0, 2);//f     2c-->  2p2c  \
             $this->turn(0, 0, 1);//f1c   1c<--  2p1c  | 3. In this step we moved fisher to another side.
@@ -325,7 +320,21 @@ class PowerPlay extends Play
         }
     }
 
-    //not atomic iteration
+    function move_child()
+    {
+        if ($this->enough(0,0,2,0,0,0))
+        {
+            $this->turn(0, 0, 2);//      2c-->  f2p2c - We have 2 children and we just move them to another side.
+            $this->turn(0, 0, 1);//f     f <--  2p2c  - We should move fisher back. As you see the task now is as simple as possible. Congratulation! The achievement is unlocked! :)
+            trace('Strategy: 2 turns step: 1 child moved');
+        }
+        else
+        {
+            throw new Exception('Step failed: Not enough children on current riverside');
+        }
+    }
+
+    //iteration with side effect
     function move_2child_and_fisher_back()
     {
         if ($this->enough(0,0,2,1,0,0))
@@ -337,20 +346,6 @@ class PowerPlay extends Play
         else
         {
             throw new Exception('Step failed: Not enough passengers on riversides');
-        }
-    }
-
-    function move_child()
-    {
-        if ($this->enough(0,0,2,0,0,0))
-        {
-            $this->turn(0, 0, 2);//      2c-->  f2p2c - We have 2 children and we just move them to another side.
-            $this->turn(0, 0, 1);//f     f <--  2p2c  - We should move fisher back. As you see the task now is as simple as possible. Congratulation! The achievement is unlocked! :)
-            trace('Strategy: 2 turns step: 2 children moved');
-        }
-        else
-        {
-            throw new Exception('Step failed: Not enough children on current riverside');
         }
     }
 }
@@ -366,6 +361,7 @@ $play = new PowerPlay(
         new Mother(),
 
         new Son(),
+        new Daughter(),
         new Daughter()
     ),
     array()
